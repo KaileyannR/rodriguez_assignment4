81af73c55c1a426d00162abc8f73b95c71d0b080
Kaileyann Rodriguez <60050415+KaileyannR@users.noreply.github.com>
2020-03-29 23:34:00 -0400

    Update Application.java

--- rodriguez_problem2/src/Application.java
+++ rodriguez_problem2/src/Application.java
@@ -1,1 +1,20 @@
+// Kaileyann Rodriguez
+// COP 3330
+// Programming Assignment 4
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Application
+{
+	public static void main(String[] args)
+	{
+		DuplicateCounter dup = new DuplicateCounter();
+		dup.count("problem2.txt");
+		dup.write("unique_wordcount.txt");
+	}
+}
