1f0e2c16f7d1e72f8f8574879ed9011cf98b0b85
Kaileyann Rodriguez <60050415+KaileyannR@users.noreply.github.com>
2020-03-29 22:51:26 -0400

    Update Application.java

--- rodriguez_problem1/src/Application.java
+++ rodriguez_problem1/src/Application.java
@@ -1,1 +1,20 @@
+// Kaileyann Rodriguez
+// COP 3330
+// Programming Assignment 4
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Application
+{
+	public static void main(String[] args)
+	{
+		DuplicateRemover app = new DuplicateRemover();
+		app.remove("problem1.txt");
+		app.write("unique_words.txt");
+	}
+}
